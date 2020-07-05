78409a54f95f9d5a03cdd278e6977163f2b56b46
beerisgood <beerisgood@users.noreply.github.com>
2020-07-05 10:53:34 +0200

    find-unsafe-dirs.ps1

---
+++ find-unsafe-dirs.ps1
@@ -0,0 +1,31 @@
+﻿# Möglichst zufälligen Dateinamen wählen.
+$executableFilename = 'psancxtqkbgfomb.cmd';
+
+# Bei Fehlern einfach fortsetzen.
+$ErrorActionPreference = 'SilentlyContinue';
+
+Get-ChildItem $env:windir -Recurse -Force | Where-Object { $_.PSIsContainer } | ForEach-Object {
+    $currentFolder = $_.Fullname;
+    $executableFilepath = (Join-Path $currentFolder $executableFilename);
+
+    # Datei mit trivialem Inhalt erstellen.
+    New-Item -Path $executableFilepath -ItemType file -Value "@echo off" | Out-Null;
+
+    if ($?) {
+        # Mehr Rechte für die neue Datei.
+        $rule = New-Object System.Security.AccessControl.FileSystemAccessRule ("BUILTIN\Users", "ExecuteFile", "Allow");
+        $acl = Get-ACL -Path $executableFilepath;
+        $acl.AddAccessRule($rule);
+        Set-ACL -Path $executableFilepath -AclObject $acl;
+
+        # Datei ausführen.
+        & $executableFilepath;
+
+        if ($?) {
+            Write-Host $currentFolder -BackgroundColor Red;
+        }
+
+        # Angelegte Datei wieder löschen.
+        Remove-Item -Path $executableFilepath;
+    }
+}
